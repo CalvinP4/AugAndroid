baf7f573cf38c0851215be10b8f113f9bed6f380
CalvinP4 <calvincp28@gmail.com>
2021-07-10 13:08:51 +0530

    Change activity order

--- app/src/main/java/com/example/arfirst/LoginActivity.java
+++ app/src/main/java/com/example/arfirst/LoginActivity.java
@@ -2,13 +2,24 @@ package com.example.arfirst;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private Button btnChangeActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btnChangeActivity = findViewById(R.id.btn_Temp);
+
+        btnChangeActivity.setOnClickListener(v -> {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        });
     }
 }
